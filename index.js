4e961522b174a8b3e12b1740d1eda8348e17d275
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-28 05:27:23 +0600

    new data added

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const faqs = require('/data/faqs.json');
 
 app.get('/', (req, res) => {
   res.send('FAQ API Running');
 });
 
-app.get('/faq-courses', (req, res) => {
+app.get('/faqs-courses', (req, res) => {
   res.send(courses);
+});
+
+app.get('/faqs/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedFaqs = faqs.find(f => f._id === id);
+  res.send(selectedFaqs);
 });
 
 app.listen(port, () => {
